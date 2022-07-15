90d0fb968060c28f39eaf3874a69dca9fe2ae598
Stefan Janssen <stefan.m.janssen@gmail.com>
2022-07-15 10:54:48 +0200

    first proper test

--- traitator/tests/test_qctraits.py
+++ traitator/tests/test_qctraits.py
@@ -1,14 +1,41 @@
 from unittest import TestCase, main
+import pandas as pd
+
+
+def _cols_check(table: pd.DataFrame, cols: [str]):
+    assert all([c in table.columns for c in cols]), \
+        "some columns in 'cols' are not in your table!"
+
+def _find_nan_rows(table: pd.DataFrame, cols: [str]) -> pd.DataFrame:
+    _cols_check(table, cols)
+    
+    errors = []
+    for col in cols:
+        errors.append(table[~pd.notnull(table[col])])
+
+    return pd.concat(errors)
+
 
 class QCTests(TestCase):
     def setUp(self):
-        pass
+        self.fp_guitar = 'traitator/tests/data/trait_data_n10906.csv'
+        self.fp_tresor = 'data/traitdata_tresor.csv'
+        self.cols_binomial = ['Genus', 'Species']
 
     def tearDown(self):
         pass
 
-    def test_singleword(self):
-        print("Hallo")
+    def test_qctresor(self):
+        tresor = pd.read_csv(self.fp_tresor, sep="\t")
+
+        # test dimension of data
+        self.assertEqual(tresor.shape, (370479, 5))
+
+        obs = _find_nan_rows(tresor, self.cols_binomial)
+        self.assertTrue(
+            obs.shape[0] == 0,
+            msg="all Genus and Species names shoul be non-emptry:\n%s" % obs)
+
 
 if __name__ == '__main__':
     main()
