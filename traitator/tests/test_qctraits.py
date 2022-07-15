dd7b700df3f4473f9f7154239bcddde60c306e3e
Stefan Janssen <stefan.m.janssen@gmail.com>
2022-07-15 10:32:54 +0200

    dummy test

---
+++ traitator/tests/test_qctraits.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, main
+
+class QCTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_singleword(self):
+        print("Hallo")
+
+if __name__ == '__main__':
+    main()
