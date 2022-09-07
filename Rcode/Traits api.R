2c07b05dfb13921bab6d5c8fbbfe37dc87d6ef55
GuiraudTresor <80392590+GuiraudTresor@users.noreply.github.com>
2022-09-08 00:39:34 +0200

    Add files via upload

--- Rcode/Traits api.R
+++ Rcode/Traits api.R
@@ -1,4 +1,3 @@
-# Traits Rscripts from John Guittar - Modifications done by Guiraud Tresor
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 ## OPTION 1 - USING THE R-PACKAGE BACDIVE
 #+++++++++++++++ BACDIVE API WEB SERVICE +++++++++++++++++++++++++++++++++#
@@ -138,6 +137,10 @@ bac <- bac %>%
   filter(!is.na(val)) %>%
   mutate(val = as.numeric(val))
 
+# split the specie column into Genus and Species 
+bac <- separate(bac, Species, c("Genus","Species"),fill = "left") %>% 
+  select(2,3,1,4)
+
 ################################################################################
 ## OPTIONAL: Retreive bacdive ID
 #+ Getting the number of strains per species - using request_bac func
@@ -154,37 +157,80 @@ bac <- bac %>%
 # get Genome size, GC content, and Gene Number from NCBI ftp site.
 # https://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt
 # download procaryote traits from link above.
+
 library(tidyverse)
 library(data.table)
 
-genos <- fread("C:/Users/Anwender/Documents/prokaryotes.txt") %>% 
+genos <- fread("C:/Users/Anwender/Documents/prokaryotes.txt")
+genos <- genos[order(genos$`#Organism/Name`),]
+genos <- genos %>% 
   filter(Status == 'Complete Genome') %>%
   transmute(
     sp = gsub("'|\\[|\\]", "", `#Organism/Name`),
-    Genus = sapply(sapply(sp, strsplit, ' '), function(x) x[1]),
-    Species = sapply(sapply(sp, strsplit, ' '), function(x) x[2]),
+    Genus = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), function(x) paste0(x[1]," ",x[2])),
+                   sapply(sapply(sp, strsplit, ' '), function(x) x[1])),
+    Species = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), function(x) x[3]),sapply(sapply(sp, strsplit, ' '), function(x) x[2])),
     Genome_Mb = as.numeric(ifelse(`Size (Mb)` == '-', NA, `Size (Mb)`)),
     GC_content = `GC%`, 
-    Gene_number = as.numeric(ifelse(Genes == '-', NA, Genes))) %>% 
+    Gene_number = as.numeric(ifelse(Genes == '-', NA, Genes))) %>%
+  mutate(Species = gsub("endosymbiont","", Species)) %>% 
   gather(trait, val, Genome_Mb, GC_content, Gene_number) %>%
   select(-sp) %>%
-  filter(!is.na(val))
+  filter(!(is.na(val)|is.na(Species)|is.na(Genus)))
+
+# convert val column in genos to numeric - before merging to genos1 db
+genos$val <- as.numeric(genos$val)
 
 # Downloaded another NCBI database from the link below
 #https://www.ncbi.nlm.nih.gov/genome/browse/#!/overview/
-# after that, the 02 Databases were merged
 
-genos2 <- fread('C:/Users/Anwender/Downloads/genomes.csv') %>%
+genos1 <- fread("C:/Users/Anwender/Downloads/prokaryotes.csv")
+genos1 <- genos1[order(genos1$`#Organism Name`),]
+genos1 <- genos1[-c(1:2,30083:30121),]%>% 
+  transmute(
+    sp = gsub("'|\\[|\\]", "", `#Organism Name`),
+    Genus = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), function(x) paste0(x[1]," ",x[2])),
+                                                 sapply(sapply(sp, strsplit, ' '), function(x) x[1])),
+    Species = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), function(x) x[3]),sapply(sapply(sp, strsplit, ' '), function(x) x[2])),
+    Genome_Mb = as.numeric(ifelse(`Size(Mb)` == '-', NA, `Size(Mb)`)),
+    GC_content = `GC%`) %>% 
+  mutate(Species = gsub("endosymbiont","", Species)) %>% 
+  gather(trait, val, Genome_Mb, GC_content) %>%
+  select(-sp) %>%
+  filter(!(is.na(val))|is.na(Species)|is.na(Genus))
+
+## Still downloaded from ncbi
+# also found this ncbi database from the website
+#https://www.ncbi.nlm.nih.gov/genome/browse/#!/overview/
+genos2 <- fread('C:/Users/Anwender/Downloads/genomes.csv') 
+
+# Select just Bacteria using the Organism groups column
+genos2 <- subset(genos2, grepl("Bacteria.*",genos2$`Organism Groups`))
+
+# Descending order using genus names
+genos2 <- genos2[order(genos2$`#Organism Name`),]
+
+genos2 <- genos2 %>% 
   transmute(sp = `#Organism Name`, val = `Size(Mb)`) %>%
-  mutate(sp = gsub(' bacterium.*', '', sp),
-         sp = gsub("'.*", "", sub("'?", "", sp))) %>%
+  mutate(sp = gsub(" bacterium.*|\\[|\\]", '', sp),
+         sp = gsub("'.*", "", sub("'?", "", sp)),
+         sp = gsub("Type-.*|unicellular.*|unidentified.*|uncultured.*|SAR.*|SCandidatus.*|Vaccinium.*|endosymbiont.*|subdivision.*",
+                   "",sp)) %>%
   filter(!grepl('\\d', sp)) %>%
-  mutate(sp = ifelse(str_count(sp, "\\S+") > 1, word(sp, 1, 2), sp)) %>%
-  separate(sp, c("Genus", "Species"), " ", fill = 'right') %>%
+  mutate(Genus = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), function(x) paste0(x[1]," ",x[2])),
+                 sapply(sapply(sp, strsplit, ' '), function(x) x[1])),
+        Species = ifelse(grepl("Candidatus",sp),sapply(sapply(sp, strsplit, ' '), 
+                   function(x) x[3]),sapply(sapply(sp, strsplit, ' '), function(x) x[2])))%>%
   transmute(Genus, Species, trait = 'Genome_Mb', val)
+genos2 <- filter(genos2, !(is.na(Genus) | is.na(Species)| Species == "x"))
 
-genos <- bind_rows(genos, genos2)
+# merge all 03 databases
+genos <- bind_rows(genos,genos1,genos2)
+# remove from the Genus column - Candidatus
+genos <- genos %>% mutate(Genus = gsub("Candidatus","",Genus))
 
+#  mutate(sp = ifelse(str_count(sp, "\\S+") > 1, word(sp, 1, 2), sp)) %>%
+#separate(sp, c("Genus", "Species"), " ", fill = 'right')
 ##########################################################################
 #+++++++++++++++++ GOLD DATABASE ++++++++++++++++++++++++++++++++++++++++#
 ##########################################################################
@@ -205,11 +251,17 @@ gold_DB <- gold_DB %>%
             Spore = `ORGANISM SPORULATION`)
 # Fix taxonomy and filter out unclassified species
 gold_DB <- gold_DB %>% 
-  mutate(Species = gsub("\\[|\\]", "",Species),
-        Species = gsub(' bacterium.*|uncultured.*', '',Species)) %>%
-  separate(Species, c("Genus","Species"),fill = "right") %>%
-  mutate(Species = gsub("sp", "", Species)) %>% 
-  filter(!is.na(Genus),!is.na(Species),Species != "")
+  mutate(Species = gsub("\\[|\\]|'", "",Species),
+        Species = gsub(' bacterium.*|uncultured.*|candidate.*', '',Species)) %>%
+  mutate(Genus = ifelse(grepl("Candidatus",Species),sapply(sapply(Species, strsplit, ' '), function(x) paste0(x[1]," ",x[2])),
+                        sapply(sapply(Species, strsplit, ' '), function(x) x[1])),
+         Species = ifelse(grepl("Candidatus",Species),sapply(sapply(Species, strsplit, ' '), 
+                                                        function(x) x[3]),sapply(sapply(Species, strsplit, ' '), function(x) x[2])))%>%
+  mutate(Species = gsub("sp.?|endosymbiont", "", Species)) %>% 
+  filter(!(is.na(Genus)|is.na(Species)))
+
+# remove empty spaces in species column
+gold_DB <- gold_DB %>%  filter(Species!= "")
 
 # Fix the width - take averages of ranges when necessary.
 gold_DB <- gold_DB %>%
@@ -282,7 +334,9 @@ gold_DB <- gold_DB %>%
 
 jgi <- gold_DB %>%
   gather(trait, val, -Genus, -Species) %>%
-  filter(!is.na(val))
+  filter(!is.na(val)) %>% 
+  select(Genus,Species,trait,val) 
+ 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 ##################################################################################
 #+++++++++++++++++++++++++++++++++++++++ IJSEM ++++++++++++++++++++++++++++++++++#
@@ -330,9 +384,9 @@ ijsem$Salt_optimum<-sapply(ijsem$Salt_optimum, simplify=T, function(x){mean(swan
 ############## Now my additional edits to the IJSEM data
 #Assign oxygen score
 ijsem$Oxygen_score <- c(5,4,3,2,1)[match(ijsem$Oxygen, 
-                                         c('obligate aerobe','microaerophile','facultative anaerobe','facultative aerobe','anaerobic'))]
+                                         c('obligate aerobe','microaerophile','facultative aerobe','facultative anaerobe','anaerobic'))]
 
-#trun Aggregation into a binary
+#turn Aggregation into a binary
 ijsem$Aggregation <- c(0,1,1)[match(ijsem$Aggregation, c('none','chain','clump'))]
 
 #clean Length and Width
@@ -464,6 +518,11 @@ ijsem <- select(ijsem, Genus, Species, GC_content = GC,
   filter(!Genus %in% c("10.1099/ijs.0.006320-0","10.1099/ijs.0.02424-0")) %>%
   mutate_if(is.factor, as.character)
 
+# Fix the Species column - checking the species names
+ijsem <- ijsem %>% 
+  mutate(Species = trimws(Species),
+         Species = ifelse(str_count(Species,"\\w+") > 1,sapply(sapply(Species, strsplit, '\\s+'), function(x) x[2]),Species))
+
 ###########################################################
 # now, sporulation data from Browne et al 2016
 # https://www.nature.com/articles/nature17645#supplementary-information 
@@ -528,8 +587,6 @@ rrnDB.5.7 <- read.delim("C:/Users/Anwender/Documents/rrnDB-5.7.tsv") %>%
   filter(!is.na(val)) %>%
   mutate(Species = ifelse(is.na(Species), 'unclassified', Species))
 
-# convert val column in genos to numeric - before combining all data frames
-genos$val <- as.numeric(genos$val)
 # put it all together
 x <- bind_rows(
   mutate(ijsem, source = 'IJSEM'),
@@ -553,16 +610,18 @@ c <- ggplot(x, aes(y = val)) + geom_boxplot() + facet_wrap(~trait, scales = 'fre
 # identifying and removing outliers
 # remove inordinately large values...
 
+x <- x %>% 
+  filter(Genus!="")
 x <- x %>%
-  filter(!(trait == 'Copies_16S' & val > 20)) %>%
-  filter(!(trait == 'GC_content' & (val > 80 | val < 20))) %>%
-  filter(!(trait == 'Gene_number' & val > 11000)) %>%
-  filter(!(trait == 'Genome_Mb' & val > 14)) %>%
-  filter(!(trait == 'Length' & val > 30)) %>%
+  filter(!(trait == 'Copies_16S' & val > 20.0)) %>%
+  filter(!(trait == 'GC_content' & (val > 80.0 | val < 20.0))) %>%
+  filter(!(trait == 'Gene_number' & val > 11000.0)) %>%
+  filter(!(trait == 'Genome_Mb' & val > 14.0)) %>%
+  filter(!(trait == 'Length' & val > 30.0)) %>%
   filter(!(trait == 'pH_optimum' & val < 2.5)) %>%
-  filter(!(trait == 'Salt_optimum' & val > 25)) %>%
-  filter(!(trait == 'Temp_optimum' & val > 80)) %>%
-  filter(!(trait == 'Width' & val > 8))
+  filter(!(trait == 'Salt_optimum' & val > 25.0)) %>%
+  filter(!(trait == 'Temp_optimum' & val > 80.0)) %>%
+  filter(!(trait == 'Width' & val > 8.0))
 
 print(paste(x_unclean - nrow(x), "outliers of", nrow(x), "trait value data points were removed"))
 
@@ -572,13 +631,20 @@ x <- x %>%
   mutate(
     Genus = trimws(Genus),
     Genus = gsub('\\[|\\]', '', Genus),
-    Genus = ifelse(Genus == 'Sul¥å\u0081tobacter', 'Sulfitobacter', Genus),
+    Genus = gsub('Sul<a5><e5><81>tobacter', 'Sulfitobacter', Genus),
     Genus = ifelse(grepl('Type-', Genus), NA, Genus),
     Genus = gsub('-.*', '', Genus),
     Species = ifelse(grepl('_| ', Genus), gsub('.*_', '', Genus), Species),
     Genus = gsub('_.*| .*', '', Genus),
     Species = trimws(Species),
-    Species = ifelse(grepl('(?!")[[:punct:]]', Species, perl = TRUE), 'unclassified', Species))
+    Species = ifelse(grepl('(?!")[[:punct:]]|sp.?', Species, perl = TRUE), 'unclassified', Species))
+
+#remove unclassified and NAs
+x <- x[x$Genus != "",] %>%
+  filter(!(is.na(Genus) | Genus == 'unclassified')) %>%
+  filter(!(is.na(Species) | Species == 'unclassified'))
+x <- x %>% filter(Species!="")
+x <- x[x$Species!="AB1",]
 
 #for plotting comparisons by source
 x_by_source <- x %>%
@@ -603,7 +669,8 @@ trait_sources <- x_by_source %>%
 x <- x %>%
   group_by(Genus, Species, trait) %>%
   summarise(val = ifelse(trait[1] %in% c('Length','Width'), log(mean(val)), mean(val))) %>%
-  spread(trait, val)
+  spread(trait, val) 
+  View(gather(x,trait, val, -Genus, -Species))
 
 # Merge spore scores (if no score and ijsem says 0, then 0; 
 # otherwise the median of spore score when we know for ijsem spore == 1
@@ -619,18 +686,6 @@ x <- x %>%
   filter(!(is.na(Genus) | Genus == 'unclassified')) %>%
   filter(!(is.na(Species) | Species == 'unclassified'))
 
-# headers from the LTP fasta file using command line
-#grep ">" LTPs132_SSU_compressed.fasta | cut -f 6,7 > LTP_headers
-
-# trying to find out which species exist both,
-# in my trait table as in the LTP
-x <- x %>% 
-  mutate(Organism = paste0(Genus," ",Species))
-LTP_headers <- read.delim("C:/Users/Anwender/Downloads/LTP_headers", header=FALSE)
-
-# match ltp to trait table by common genus and species name
-z <- merge(x,LTP_headers, by.x = 'Organism',by.y = 'V1')
-
 #remove taxa not present in the Living tree Project or the Silva-derived taxonomy file from our usearch pipeline
 tmp <- bind_rows(select(tax_succ, Genus, Species), select(tax_LTP, Genus, Species)) %>%
   filter(!(Genus == 'unclassified' | Species == 'unclassified'))
@@ -639,17 +694,88 @@ x <- filter(x, paste(Genus, Species) %in% paste(tmp$Genus, tmp$Species))
 ###save RDS
 saveRDS(x, file = 'data\\traits_sparse.RDS')
 saveRDS(trait_sources, file = 'data\\trait_sources.RDS')
+#++++++++++++++++++++++
+#+ Import and read tree from LTP
+library(phytools)
+library(ape)
+library(maps)
+ltp_tree <- read.newick('C:/Users/Anwender/Downloads/LTPs132_SSU_tree.newick')
+
+#ltp meta table
+ltp_meta <- read.csv('C:/Users/Anwender/Downloads/LTPs132_SSU.csv',sep = "\t",header = F)
+
+# trying to find out which species exist both,
+# in my trait table as in the LTP
+x <- x %>% 
+  mutate(Organism = paste0(Genus," ",Species))
+
+LTP_headers <- read.delim("C:/Users/Anwender/Downloads/LTP_headers", header=FALSE)
+# attribute colnames to LTP
+colnames(LTP_headers)<- c("Species","Family")
+# reorder LTP
+LTP_headers <- LTP_headers %>% arrange(Species)
+# match ltp to trait table
+z <- merge(x,LTP_headers, by.x = 'Organism',by.y = 'V1')
+#########################################################
+# ++ First Trait version
+# arrange format in val column
+# int col
+c <- c('Oxygen_tolerance','Spore','Gram_positive','Gene_number','Motility','Aggregation_score','Copies_16S','Spore_score','B_vitamins')
+traitdata_tresor$val[traitdata_tresor$trait %in% c] <- as.integer(traitdata_tresor$val[traitdata_tresor$trait %in% c])
+# double col
+d <- c('GC_content','Genome_Mb','IgA','Length','pH_optimum','Salt_optimum','Temp_optimum','Width')
+traitdata_tresor$val[traitdata_tresor$trait %in% d] <- as.double(traitdata_tresor$val[traitdata_tresor$trait %in% d])
+
+# checking if d vector are all doubles i.e contains atleast one decimal number
+traitdata_tresor$val[traitdata_tresor$trait %in% d]<-ifelse(!grepl('\\.',traitdata_tresor$val[traitdata_tresor$trait %in% d]),
+                                                            paste0(traitdata_tresor$val[traitdata_tresor$trait %in% d],".",0),
+                                                            traitdata_tresor$val[traitdata_tresor$trait %in% d])
+
+# round decimal places in pH_opt col to 2 decimal places
+traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"] <- round(as.numeric(traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"]),digits = 2)
+# check presence of decimal point, adding . and 2 decimal places
+traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"] <- ifelse(!grepl('\\.',traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"]),
+                                                                     paste0(traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"],".",00),
+                                                                     traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"])
+
+traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"] <- ifelse(grepl('\\.\\d{1}$',traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"]),
+                                                                     paste0(traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"],0),
+                                                                     traitdata_tresor$val[traitdata_tresor$trait=="pH_optimum"])
+# sort
+traitdata_tresor <- traitdata_tresor[order(traitdata_tresor$Genus,traitdata_tresor$Species,traitdata_tresor$trait,traitdata_tresor$source),]
+
+# +++ Latest trait version
+# arrange format in val column
+# int col
+c <- c('Oxygen_tolerance','Spore','Gram_positive','Gene_number','Motility','Aggregation_score','Copies_16S','Spore_score','B_vitamins')
+y$val[y$trait %in% c] <- as.integer(y$val[y$trait %in% c])
+# double col
+d <- c('GC_content','Genome_Mb','IgA','Length','pH_optimum','Salt_optimum','Temp_optimum','Width')
+y$val[y$trait %in% d] <- as.double(y$val[y$trait %in% d])
+
+# checking if d vector are all doubles i.e contains atleast one decimal number
+y$val[y$trait %in% d]<-ifelse(!grepl('\\.',y$val[y$trait %in% d]),
+                                                            paste0(y$val[y$trait %in% d],".",0),
+                                                            y$val[y$trait %in% d])
+
+# sort table using genus,species,trait and finally source
+y <- y[order(y$Genus,y$Species,y$trait,y$source),]
+
+# arrange traitdata_tresor specie column
+traitdata_tresor <- separate(traitdata_tresor, Species, c('Genus1','Species'),fill = "left") %>% 
+  select(1,3,4,5,6)
+
+# write file in a tsv file
+library(readr)
+write_csv(traitdata_tresor,'C:\\Users\\Anwender\\Downloads\\traitdata_tresor',append = F)
+write_tsv(y,'C:\\Users\\Anwender\\Downloads\\traitdata_tresor_2.tsv',append = F)
+
+# write in xlsx file
+library(writexl) #export data from R format to Excel format
+write_xlsx(traitdata_tresor, 'C:\\Users\\Anwender\\Downloads\\traitdata_tresor')
+x <- select(x,1,2,3,5,4)
+
+write_tsv(x,'C:\\Users\\Anwender\\Downloads\\traitdata_tresor.tsv',append = F)
 
 
-#library(writexl) export data from R format to Excel format
-#write_xlsx(trait_wide, 'C:\\Users\\Anwender\\Downloads\\Traits_wide.xlsx')
-#library(reshape2)
-trait_wide <- dcast(x, Genus + Species ~ trait, value.var="val")
 
-################################
-#+ merge search_taxon and gold db via common genus & species names
-#View(left_join(search_taxon,gold_DB,by=c('Genus','Species'))%>% summarize(n=n()))
-#+ grouping same genus & species together - will be used after digitizing traits 
-#View(filter(gold_DB,Genus != "") %>% group_by(Genus,Species) %>% summarize(n=n(),.groups = 'keep'))
-
-# sp <- gsub("[[]]", "",gold_DB$Species)
